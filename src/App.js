02e8ca50d5551ff4cd9429fdc4adc39e366e8e3b
brijendra2099 <37235955+brijendra2099@users.noreply.github.com>
2020-04-14 00:14:17 +0530

    removed logo

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />*/}
         {this.state.monsters.map((monster) => (
           <h1 key={monster.id}> {monster.name} </h1>
         ))}

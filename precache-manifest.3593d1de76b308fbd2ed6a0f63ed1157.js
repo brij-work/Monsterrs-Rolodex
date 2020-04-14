e3aacd30313d33faeb73561a614dc656d5248b2b
brij-work <63343224+brij-work@users.noreply.github.com>
2020-04-15 00:32:50 +0530

    Updates

---
+++ precache-manifest.3593d1de76b308fbd2ed6a0f63ed1157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629f010cd8b20930132ffe6009676e26",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "08abda87a0a2c9a69d0e",
+    "url": "/Monsters-Rolodex/static/css/main.4c39c4c7.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/Monsters-Rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08abda87a0a2c9a69d0e",
+    "url": "/Monsters-Rolodex/static/js/main.899d58d1.chunk.js"
+  },
+  {
+    "revision": "5bb244a38e825d0ab28e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.129d40ef.js"
+  }
+]);

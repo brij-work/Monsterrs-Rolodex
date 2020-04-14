773fb247d779a873d2fc24951b81c6a10f9a0ee5
brij-work <63343224+brij-work@users.noreply.github.com>
2020-04-14 16:46:56 +0530

    added working search monster component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -18,13 +19,20 @@ class App extends Component {
   }
 
   render() {
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
       <div className="App">
-        <CardList>
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}> {monster.name} </h1>
-          ))}
-        </CardList>
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })
+          }
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
